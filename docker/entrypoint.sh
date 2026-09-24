8ab74f4a27ea3a275569f6c5c65f51ba2d95c125
INAPP <imapp.help@gmal.com>
2026-09-23 18:19:08 -0700

    fix: properly escape quotes in Apache Substitute directive

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -26,7 +26,7 @@ chown apache:root /var/www/html/database 2>/dev/null || true
 # Runs at HTTP response level, bypassing Railway's build cache entirely.
 echo 'LoadModule substitute_module modules/mod_substitute.so' > /etc/apache2/conf.d/load-substitute.conf
 echo 'AddOutputFilterByType SUBSTITUTE text/html' > /etc/apache2/conf.d/substitute.conf
-printf '%s\n' 'Substitute "s|<html>|<html data-theme=light>|i"' >> /etc/apache2/conf.d/substitute.conf
+printf '%s\n' 'Substitute "s|<html>|<html data-theme=\"light\">|i"' >> /etc/apache2/conf.d/substitute.conf
 
 # Clear Laravel view cache to ensure blade template changes take effect
 cd /var/www/html && php artisan view:clear 2>/dev/null || true
